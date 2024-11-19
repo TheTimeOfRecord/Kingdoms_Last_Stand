60c4cbe1d53ecf7fc1057f8c2e124fe1153c4b35
SBHObject <168382040+SBHObject@users.noreply.github.com>
2024-11-19 17:11:48 +0900

    Complex TowerStatsSystem
    Feat AttackTypeStat
    Feat TowerStats
    Feat TowerStatsHandler

--- Assets/01. Scripts/Towers/AttackTypeStat.cs
+++ Assets/01. Scripts/Towers/AttackTypeStat.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackTypeStat : MonoBehaviour
+public class AttackTypeStat
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int upgradeCount = 0;
+    public bool hasType = false;
+    public float currentDamage = 0;
+    public AttackTypeStatSO statData;
 
-    // Update is called once per frame
-    void Update()
+    public AttackTypeStat(AttackTypeStatSO dataSO)
     {
-        
+        statData = dataSO;
     }
 }
--- Assets/01. Scripts/Towers/TowerStats.cs
+++ Assets/01. Scripts/Towers/TowerStats.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerStats : MonoBehaviour
+public class TowerStats
 {
-    // Start is called before the first frame update
-    void Start()
+    //포탑이 가진 공격속성
+    public List<AttackTypeStat> typeStats = new List<AttackTypeStat>();
+
+    //포탑 사거리, 공격속도
+    public float currentRange;
+    public float currentRate;
+
+    /// <summary>
+    /// 포탑 기본 사거리, 공격속도 지정 메서드
+    /// </summary>
+    /// <param name="range"></param>
+    /// <param name="rate"></param>
+    public void InitTowerData(float range, float rate)
     {
-        
+        currentRange = range;
+        currentRate = rate;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void InitAttackStats(AttackTypeStatListSO listData)
     {
-        
+        foreach(var statSO in listData.AttackLists)
+        {
+            typeStats.Add(new AttackTypeStat(statSO));
+        }
     }
 }
